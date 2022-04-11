6808b5f449be9ae204cba8ff5c0b411ceaf40e3b
barbadeadesh <barbadeadesh2020@email.com>
2022-04-11 18:44:28 +0530

    ADDED:Functionality to return happy or sad

---
+++ UC1_HappySadReturn.java
@@ -0,0 +1,21 @@
+package com.bridgelab.moodanlyzer;
+
+public class MoodAnalyser {
+	private String message;
+
+	public MoodAnalyser() {
+
+	}
+
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		if (message.contains("Sad"))
+			return "SAD";
+		else
+			return "HAPPY";
+	}
+
+}
